bdd873ab651b60b3b339fa4f19d78dc45a15f0c0
uki <yuqi.xiao@student.ubc.ca>
2022-11-22 12:05:30 -0800

    server is working in attempt2java

--- Try/app/src/main/java/com/example/atry/MainActivity.java
+++ Try/app/src/main/java/com/example/atry/MainActivity.java
@@ -1,59 +1,92 @@
 package com.example.atry;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
-    // declare attribute for textview
-    private TextView pagenameTextView;
+    private String url = "http://" + "10.43.65.250" + ":" + 5000 + "/";
+    private String postBodyString;
+    private MediaType mediaType;
+    private RequestBody requestBody;
+    private Button connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pagenameTextView = findViewById(R.id.pagename);
+        connect = findViewById(R.id.connect);
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postRequest("your message here", url);
 
-        // creating a client
+            }
+        });
+
+
+    }
+
+    private RequestBody buildRequestBody(String msg) {
+        postBodyString = msg;
+        mediaType = MediaType.parse("text/plain");
+        requestBody = RequestBody.create(postBodyString, mediaType);
+        return requestBody;
+    }
+
+
+    private void postRequest(String message, String URL) {
+        RequestBody requestBody = buildRequestBody(message);
         OkHttpClient okHttpClient = new OkHttpClient();
-
-        // building a request
-        Request request = new Request.Builder().url("http://10.43.65.250:5000").build();
-
-        // making call asynchronously
+        Request request = new Request
+                .Builder()
+                .post(requestBody)
+                .url(URL)
+                .build();
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
-            // called if server is unreachable
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+            public void onFailure(final Call call, final IOException e) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(MainActivity.this, "server down", Toast.LENGTH_SHORT).show();
-                        pagenameTextView.setText("error connecting to the server");
+
+
+                        Toast.makeText(MainActivity.this, "Something went wrong:" + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        call.cancel();
+
+
                     }
                 });
+
             }
 
             @Override
-            // called if we get a
-            // response from the server
-            public void onResponse(
-                    @NotNull Call call,
-                    @NotNull Response response)
-                    throws IOException {pagenameTextView.setText(response.body().string());
+            public void onResponse(Call call, final Response response) throws IOException {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Toast.makeText(MainActivity.this, response.body().string(), Toast.LENGTH_LONG).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
             }
         });
     }
