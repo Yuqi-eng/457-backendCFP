eab0bed6335c47958a3a8fb84f717dc6fb4d9c4f
oliviamelaniputrii <olivia.melani@yahoo.com>
2023-02-09 12:52:01 -0800

    added simulink Android Studio

---
+++ TestUntitledAS/src/main/java/com/example/untitled/OnFragmentInteractionListener.java
@@ -0,0 +1,9 @@
+package com.example.untitled;
+
+
+interface OnFragmentInteractionListener {
+   void onFragmentCreate (String name);
+   void onFragmentStart  (String name);
+   void onFragmentPause  (String name);
+   void onFragmentResume (String name);
+}
