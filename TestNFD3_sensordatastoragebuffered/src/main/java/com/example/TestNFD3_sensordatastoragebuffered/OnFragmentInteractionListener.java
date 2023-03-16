c5d1e482daa295a96e93422a88d89b1420734a4d
oliviamelaniputrii <olivia.melani@yahoo.com>
2023-03-16 12:45:58 -0700

    added file with buffered data

---
+++ TestNFD3_sensordatastoragebuffered/src/main/java/com/example/TestNFD3_sensordatastoragebuffered/OnFragmentInteractionListener.java
@@ -0,0 +1,9 @@
+package com.example.TestNFD3_sensordatastoragebuffered;
+
+
+interface OnFragmentInteractionListener {
+   void onFragmentCreate (String name);
+   void onFragmentStart  (String name);
+   void onFragmentPause  (String name);
+   void onFragmentResume (String name);
+}
