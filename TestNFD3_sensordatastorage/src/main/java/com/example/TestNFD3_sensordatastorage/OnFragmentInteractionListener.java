6f9a0485f651e0958041c841ccd43b5ccca15dfe
oliviamelaniputrii <olivia.melani@yahoo.com>
2023-03-16 11:31:22 -0700

    added data storage AS file

---
+++ TestNFD3_sensordatastorage/src/main/java/com/example/TestNFD3_sensordatastorage/OnFragmentInteractionListener.java
@@ -0,0 +1,9 @@
+package com.example.TestNFD3_sensordatastorage;
+
+
+interface OnFragmentInteractionListener {
+   void onFragmentCreate (String name);
+   void onFragmentStart  (String name);
+   void onFragmentPause  (String name);
+   void onFragmentResume (String name);
+}
